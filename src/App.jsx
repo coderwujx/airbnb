b7621784c83da54c43e72cc774cf690862dc14ef
WuJXcoder <coderwujx@gmail.com>
2023-07-18 17:21:54 +0800

    首页布局分析

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React, { memo } from 'react'
 import { useRoutes } from 'react-router-dom'
 import routes from './router'
+import AppHeader from './components/AppHeader'
+import AppFooter from './components/AppFooter'
 
 const App = memo((props) => {
   return (
     <div className='app'>
-      <div className='header'>header</div>
+      <AppHeader />
       <div className='page'>
         {/* 配置路由 */}
         {useRoutes(routes)}
       </div>
-      <div className='footer'>footer</div>
+      <AppFooter />
     </div>
   )
 })
