54fbdfc5e797d8b20d49658668363f289480c87c
WuJXcoder <coderwujx@gmail.com>
2023-07-19 15:00:09 +0800

    header panel显示实现

--- src/components/AppHeader/c-cpns/HeaderRight/index.jsx
+++ src/components/AppHeader/c-cpns/HeaderRight/index.jsx
@@ -1,10 +1,31 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import { RightWrapper } from './style'
 import IconGlobal from '@/assets/svg/icon_global'
 import IconMenu from '@/assets/svg/icon_menu'
 import IconAvatar from '@/assets/svg/icon_avatar'
 
 const HeaderRight = memo(() => {
+  //panel显示状态
+  const [showPanel, setShowPanel] = useState(false)
+
+  useEffect(() => {
+    //监听window点击
+    window.addEventListener('click', () => {
+      setShowPanel(false)
+    }, true)
+    return () => {
+      window.removeEventListener('click', () => {
+        setShowPanel(false)
+      }, true)
+    }
+  }, [])
+
+  // 修改panel的显示
+
+  function profileClickHandle() {
+    setShowPanel(true)
+  }
+
   return (
     <RightWrapper>
       <div className='btns'>
@@ -15,9 +36,23 @@ const HeaderRight = memo(() => {
         </span>
       </div>
 
-      <div className='profile'>
+      <div className='profile' onClick={profileClickHandle}>
         <IconMenu />
         <IconAvatar />
+
+        {
+          showPanel && <div className="panel">
+            <div className="top">
+              <div className="item register">注册</div>
+              <div className="item login">登录</div>
+            </div>
+            <div className="bottom">
+              <div className='item'>出租房源</div>
+              <div className='item'>开展体验</div>
+              <div className='item'>帮助</div>
+            </div>
+          </div>
+        }
       </div>
     </RightWrapper>
   )
--- src/components/AppHeader/c-cpns/HeaderRight/style.js
+++ src/components/AppHeader/c-cpns/HeaderRight/style.js
@@ -26,6 +26,7 @@ export const RightWrapper = styled.div`
   }
 
   .profile{
+    position: relative;
     display: flex;
     width: 77px;
     height: 42px;
@@ -40,5 +41,33 @@ export const RightWrapper = styled.div`
     cursor: pointer;
 
     ${props => props.theme.mixin.boxShadow}
+
+    .panel{
+      position: absolute;
+      top: 60px;
+      right: 0;
+      width: 240px;
+      background-color: #fff;
+      border-radius: 10px;
+      box-shadow: 0 0 6px rgba(0,0,0,.2);
+
+      .top, .bottom{
+        padding: 10px 0;
+
+        .item{
+          height: 40px;
+          line-height: 40px;
+          padding: 0 16px;
+
+          &:hover{
+          background-color: #f5f5f5;
+        }
+        }
+      }
+
+      .top{
+          border-bottom: 1px solid #ddd;
+        }
+    }
   }
 `
