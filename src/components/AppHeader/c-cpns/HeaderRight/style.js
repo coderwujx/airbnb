732e17727421960ef0a928ca1938dd95a7a87f9e
WuJXcoder <coderwujx@gmail.com>
2023-07-19 10:31:19 +0800

    header布局初步

--- src/components/AppHeader/c-cpns/HeaderRight/style.js
+++ src/components/AppHeader/c-cpns/HeaderRight/style.js
@@ -7,7 +7,6 @@ export const RightWrapper = styled.div`
   justify-content: flex-end;
   align-items: center;
   color: ${props => props.theme.text.primaryColor};
-  font-size: 14px;
   font-weight: 600;
 
   .btns{
