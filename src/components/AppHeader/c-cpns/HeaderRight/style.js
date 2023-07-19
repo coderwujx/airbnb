41c4911f504da3ba12bed2a7fab2a811d4425e9b
WuJXcoder <coderwujx@gmail.com>
2023-07-19 10:20:04 +0800

    HeaderLeft和HeaderRight布局实现

--- src/components/AppHeader/c-cpns/HeaderRight/style.js
+++ src/components/AppHeader/c-cpns/HeaderRight/style.js
@@ -5,4 +5,41 @@ export const RightWrapper = styled.div`
   flex: 1;
   display: flex;
   justify-content: flex-end;
+  align-items: center;
+  color: ${props => props.theme.text.primaryColor};
+  font-size: 14px;
+  font-weight: 600;
+
+  .btns{
+    display: flex;
+
+    .btn{
+      height: 18px;
+      line-height: 18px;
+      padding: 12px 15px;
+      cursor: pointer;
+      border-radius: 22px;
+
+      &:hover{
+        background-color: #f5f5f5;
+      }
+    }
+  }
+
+  .profile{
+    display: flex;
+    width: 77px;
+    height: 42px;
+    margin-right: 24px;
+    justify-content: space-evenly;
+    align-items: center;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+    border-radius: 25px;
+    background-color: #fff;
+    color: ${props => props.theme.text.primaryColor};
+    cursor: pointer;
+
+    ${props => props.theme.mixin.boxShadow}
+  }
 `
