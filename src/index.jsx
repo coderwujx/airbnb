06edac26cc960f6cafe5c261e30084fe01e12d85
WuJXcoder <coderwujx@gmail.com>
2023-07-19 09:50:08 +0800

    配置主题颜色混入

--- src/index.jsx
+++ src/index.jsx
@@ -2,21 +2,25 @@ import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { ThemeProvider } from 'styled-components'
 
 import App from '@/App'
 import Loading from './views/loading'
 import 'normalize.css'
 import './assets/css/index.css'
 import store from './store'
+import theme from './assets/theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Suspense fallback={<Loading />}>
       <Provider store={store}>
-        <HashRouter>
-          <App />
-        </HashRouter>
+        <ThemeProvider theme={theme}>
+          <HashRouter>
+            <App />
+          </HashRouter>
+        </ThemeProvider>
       </Provider>
     </Suspense>
   </React.StrictMode >
